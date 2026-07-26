cb58f57e587cda0d701e986afb2b5b1150547b32
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-07-26 11:14:20 +0300

    package-validated

--- app/Http/Requests/Api/UesrSubscribeRequest.php
+++ app/Http/Requests/Api/UesrSubscribeRequest.php
@@ -3,18 +3,31 @@
 namespace App\Http\Requests\Api;
 
 use App\Http\Requests\BaseRequest\BaseRequest;
-use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Package;
+use Illuminate\Validation\Rule;
 
 class UesrSubscribeRequest extends BaseRequest
 {
-
-
     public function rules(): array
     {
         return [
-            'package_id' => 'required|exists:packages,id',
+            'package_id' => [
+                'required',
+                'exists:packages,id',
+                function ($attribute, $value, $fail) {
+                    $isFree = \App\Models\Package::where('id', $value)->value('is_free');
+                    if ($isFree) {
+                        $fail('You cannot subscribe to this package because it is a free package.');
+                    }
+                },
+            ],
+        ];
+    }
 
+    public function messages(): array
+    {
+        return [
+            'package_id.exists' => 'You cannot subscribe to this package because it is a free package.',
         ];
     }
 }

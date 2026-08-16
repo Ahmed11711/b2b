17b7ae6b14f31a4a7c11aef426e62f485885ae28
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-08-16 15:00:40 +0300

    aa

--- app/Http/Controllers/Api/ApplyPosts/AllpostsToApplayController.php
+++ app/Http/Controllers/Api/ApplyPosts/AllpostsToApplayController.php
@@ -32,16 +32,11 @@ class AllpostsToApplayController extends BaseController
     protected function applyScoping($query)
     {
         if (request()->isMethod('get')) {
-            $authUserId = auth('api')->id();
-            // $categoryIds = auth('api')->user()->categories()->pluck('category_id');
+            $categoryIds = auth('api')->user()->categories()->pluck('category_id');
 
             return $query
-                ->where('is_active', 1)
-                // ->whereIn('category_id', $categoryIds)
-                ->where('user_id', '!=', $authUserId)
-                ->whereDoesntHave('bids', function ($q) use ($authUserId) {
-                    $q->where('user_id', $authUserId);
-                });
+                ->where('is_active', true)
+                ->whereIn('category_id', $categoryIds);
         }
 
         return $query->where('user_id', auth('api')->id());

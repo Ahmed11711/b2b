5801bdb0a58fef915b19f8e3552082edd1bd7f94
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-08-16 13:42:13 +0300

    bags

--- app/Http/Resources/Admin/Bag/BagResource.php
+++ app/Http/Resources/Admin/Bag/BagResource.php
@@ -23,10 +23,10 @@ class BagResource extends JsonResource
         }
 
         $data['image'] = $this->image
-            ? url(str_replace('/storage/app/public', '/storage', $this->image))
+            ? rtrim(config('app.url'), '/') . '/' . ltrim(str_replace('/storage/app/public', 'storage', $this->image), '/')
             : null;
         $data['icon'] = $this->icon
-            ? url(str_replace('/storage/app/public', '/storage', $this->icon))
+            ? rtrim(config('app.url'), '/') . '/' . ltrim(str_replace('/storage/app/public', 'storage', $this->icon), '/')
             : null;
 
         $data['bagCategory'] = BagsCategoryResource::collection($this->whenLoaded('bagCategory'));

61923a38aefe4ad2495ce45b02d6a6017f26c7ba
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-08-18 17:06:50 +0300

    b2b

--- app/Http/Controllers/Api/Profile/ProfileAccountController.php
+++ app/Http/Controllers/Api/Profile/ProfileAccountController.php
@@ -59,9 +59,10 @@ class ProfileAccountController extends BaseController
 
     public function update(Request $request, int $id = 0): JsonResponse
     {
+        Log::info('DEBUG REQUEST', $request->all());
+        Log::info('DEBUG METHOD', [$request->method()]);
 
         $userId = auth('api')->id();
-
         return parent::update($request, $userId);
     }
 

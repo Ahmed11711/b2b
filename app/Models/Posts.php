0e90c7218f87320b433ab4277a6c97acd0c0939b
Ahmed11711 <ahmed11711@gmail.com>
2026-08-27 17:01:25 +0300

    category_id

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -8,7 +8,7 @@ class Posts extends Model
 {
 
     public array $searchable = ['description'];
-    public array $filterable = ['user_id', 'is_active'];
+    public array $filterable = ['user_id', 'is_active', 'category_id'];
     public array $allowedFields = ['id', 'user_id', 'title', 'description', 'price_from', 'price_to', 'image', 'is_active', 'created_at', 'updated_at'];
 
     //
